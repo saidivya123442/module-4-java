53d58710dfdee1076c02556f9a3e7797541011dc
saidivya123442 <saidivyayanamala17@gmail.com>
2025-11-13 22:38:28 +0530

    Implement Shape, Circle, and Rectangle classes

---
+++ Shape.java
@@ -0,0 +1,43 @@
+public class Shape {
+    void displayShape() {
+        System.out.println("This is a shape");
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+        Rectangle r = new Rectangle(4, 6);
+
+        c.displayShape();
+        c.area();
+
+        r.displayShape();
+        r.area();
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    void area() {
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}
+
+class Rectangle extends Shape {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    void area() {
+        double area = length * width;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
